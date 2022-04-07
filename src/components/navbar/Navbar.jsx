0a67162ac5f17d8e80f598da0d45f1d8be240cd1
Volodymyr <volodyaruzhak@gmail.com>
2022-04-07 19:04:50 +0300

    implement styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { RiMenu3Like, RiCloseLin } from 'react-icons/ri'
 import './navbar.css';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='gpt3__navbar'>
+            <div className='gpt3_navbar-links'>
+
+            </div>
+        </div>
     )
 }
 
