722bc9c1c43d941df148712433d9b95cf98d9302
Volodymyr <volodyaruzhak@gmail.com>
2022-05-02 15:28:57 +0300

    done some work on features.jsx and start styling features.css

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './features.css';
 
 const featuresData = [
@@ -24,13 +25,13 @@ const Features = () => {
     return (
         <div className='gpt3__features section__padding' id='features'>
             <div className='gpt3__features-heading'>
-            <h1 className='gradient__text'>The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
-            <p>Request Early Access to Get Started</p>
+                <h1 className='gradient__text'>The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
+                <p>Request Early Access to Get Started</p>
             </div>
             <div className='gpt3__features-container'>
-            {/* {featuresData.map((item, index) => ( */}
-                {/* <Feature title={item.title} text={item.text} key={item.title + index} /> */}
-            {/* ))} */}
+                {featuresData.map((item, index) => (
+                    <Feature title={item.title} text={item.text} key={item.title + index} />
+                ))}
             </div>
         </div>
     )
