5e67235dfae84fabae48cd8aaafd3e723256bdd7
Volodymyr <volodyaruzhak@gmail.com>
2022-05-30 19:44:20 +0300

    while working on styling footer found a typo in footer.jsx for the button

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <div className="gpt3__footer-heading">
                 <h1 className="gradient__text">Do you want to step in to the future before others</h1>
             </div>
-            <div className='gpt__footer-btn'>
+            <div className='gpt3__footer-btn'>
                 <p>Request Early Access</p>
             </div>
             <div className='gpt__footer-links'>
